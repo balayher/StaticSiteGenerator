473984e5601ff5966734ac503be3498e9f243693
balayher <balayher@gmail.com>
2025-02-04 17:28:11 -0800

    adding textnode to htmlnode function

--- src/main.py
+++ src/main.py
@@ -5,5 +5,6 @@ from leafnode import LeafNode
 def main():
     textnode = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
     print(textnode)
-    
+
+
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
+from leafnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -36,6 +36,29 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
         node2 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
         self.assertNotEqual(node, node2)
+
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        node2 = text_node_to_html_node(node)
+        self.assertEqual(node2.tag, "b")
+        self.assertEqual(node2.value, "This is a text node")
+
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node2 = text_node_to_html_node(node)
+        self.assertEqual(node2.tag, None)
+        self.assertEqual(node2.value, "This is a text node")
+
+        node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+        node2 = text_node_to_html_node(node)
+        self.assertEqual(node2.tag, "a")
+        self.assertEqual(node2.value, "This is a text node")
+        self.assertEqual(node2.props, {"href": "https://www.boot.dev"})
+
+        node = TextNode("This is a text node", TextType.IMAGE, "https://www.boot.dev")
+        node2 = text_node_to_html_node(node)
+        self.assertEqual(node2.tag, "img")
+        self.assertEqual(node2.value, "")
+        self.assertEqual(node2.props, {"src": "https://www.boot.dev", "alt": "This is a text node"})
+
 
 if __name__ == "__main__":
     unittest.main()
